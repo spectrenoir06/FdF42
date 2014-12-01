cda35489a0c62d01e1b3c458465ace166faffe68
Antoine DOUSSAUD <antoine@doussaud.org>
2014-12-01 19:43:58 +0100

    norme

--- main.c
+++ main.c
@@ -38,29 +38,26 @@ int		loop(t_all *all)
 	return (0);
 }
 
-t_list	*file_to_lst(char *file, t_all *all)
+void	file_to_lst(char *file, t_all *all, t_list **lst)
 {
-	t_list	*lst;
 	char	**tmp;
 	char	**tmp2;
 	char	*line;
 	t_pt3d	point;
 	int		fd;
 
-	all->map.lx = 0;
 	all->map.ly = 0;
-	lst = NULL;
 	fd = open(file, 'r');
 	while (get_next_line(fd, &line))
 	{
 		tmp = ft_strsplit(line, ' ');
 		tmp2 = tmp;
 		free(line);
-		x = 0;
+		all->map.lx = 0;
 		while (*tmp)
 		{
 			point = ft_new_point3d(all->map.lx, all->map.ly, ft_atoi(*tmp));
-			ft_lstsmartpushback(&lst, ft_lstnew(&point, sizeof(t_pt3d)));
+			ft_lstsmartpushback(lst, ft_lstnew(&point, sizeof(t_pt3d)));
 			free(*tmp);
 			tmp++;
 			all->map.lx++;
@@ -68,9 +65,6 @@ t_list	*file_to_lst(char *file, t_all *all)
 		free(tmp2);
 		all->map.ly++;
 	}
-	all->map.lx = x;
-	all->map.ly = y;
-	return (lst);
 }
 
 int		main(int argc, char **argv)
@@ -82,6 +76,7 @@ int		main(int argc, char **argv)
 	int		x;
 	int		y;
 	int		i;
+	lst = NULL;
 
 	all.env.mlx = mlx_init();
 	all.env.win = mlx_new_window(all.env.mlx, SCREEN_SIZE_X, SCREEN_SIZE_Y, "Hello world!");
@@ -90,10 +85,9 @@ int		main(int argc, char **argv)
 	x = 0;
 	y = 0;
 	i = 0;
-	printf("argc = %d\n", argc);
 	if (argc > 1)
 	{
-		lst = file_to_lst(argv[1], &all);
+		file_to_lst(argv[1], &all, &lst);
 		x = all.map.lx;
 		y = all.map.ly;
 		lsttmp = lst;
@@ -110,8 +104,6 @@ int		main(int argc, char **argv)
 			all.map.tab[point.x][point.y] = point;
 			lst = lst->next;
 		}
-		printf("x = %d, y = %d\n", x, y);
-		printf("max = %d, min = %d\n", all.map.max, all.map.min);
 		ft_lstsimpledel(&lsttmp);
 	}
 	else
